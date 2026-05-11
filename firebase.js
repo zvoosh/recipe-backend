272c003c0b7a5cbd1799b47e47795048b728a3b7
Dusan Ilic <dusan.ilic1999@gmail.com>
2026-05-11 12:47:30 +0200

    fix: fix

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./recipes.json");
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
